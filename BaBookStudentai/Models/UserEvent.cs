027e0a9875d5f296bb6f76273eb4edf8bbb5c0dc
Sandra Spiridonovaitė <sandra.spiridonovaite@gmail.com>
2017-10-09 10:37:29 +0300

    deleted time variable

--- BaBookStudentai/Models/UserEvent.cs
+++ BaBookStudentai/Models/UserEvent.cs
@@ -9,7 +9,6 @@ namespace BaBookStudentai.Models
     {
             public string Title { get; set; }
             public string Date { get; set; }
-            public string Time { get; set; }
             public string Comment { get; set; }
             public string Location { get; set; }
     }
