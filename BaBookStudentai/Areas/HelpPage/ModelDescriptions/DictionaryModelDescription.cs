b5fce3c3dc49f130fd2dd486b7efef03f65cf7bc
Vytautas Krivickas <vytautas1996@gmail.com>
2017-10-09 09:54:47 +0300

    Total Wipeout #2

--- BaBookStudentai/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace BaBookStudentai.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- BaBookStudentai/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace BaBookStudentai.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
