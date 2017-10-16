ff5b5cc30088633070748db1da0c58471fe5994a
Vytautas Krivickas <vytautas1996@gmail.com>
2017-10-16 11:27:58 +0300

    Add groups controller

---
+++ BaBookStudentai/DTOs/GroupDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BaBookStudentai.Entities;
+
+namespace BaBookStudentai.DTOs
+{
+    public class GroupDto
+    {
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+
+        internal static List<GroupDto> Convert(IQueryable<Group> groups)
+        {
+            var list = new List<GroupDto>();
+            foreach (var g in groups)
+            {
+                var groupDto = new GroupDto
+                {
+                    GroupId = g.GroupId,
+                    Name = g.Name
+                };
+                list.Add(groupDto);
+            }
+            return list;
+        }
+    }
+}
