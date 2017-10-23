ab908206dc62185a839ced556684746c114d6839
Lukas Cedronas <lukascedronas1@gmail.com>
2017-10-24 00:34:40 +0300

    Added signalr chat (server side)

---
+++ BaBookStudentai/ChatHub.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using BaBookStudentai.Common;
+
+namespace BaBookStudentai
+{
+    public class ChatHub : Hub
+    {
+
+        static List<UserDetail> ConnectedUsers = new List<UserDetail>();
+        static List<MessageDetail> CurrentMessage = new List<MessageDetail>();
+
+        public void UserDisconnect()
+        {
+            var item = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+            if (item != null)
+            {
+                ConnectedUsers.Remove(item);
+
+                var id = Context.ConnectionId;
+                Clients.All.onUserDisconnected(id, item.UserName);
+
+                Clients.All.messageReceived(item.UserName, "Has disconnected");
+            }
+        }
+
+        public void Connect(string userName)
+        {
+            var id = Context.ConnectionId;
+
+
+            if (ConnectedUsers.Count(x => x.ConnectionId == id) == 0)
+            {
+                ConnectedUsers.Add(new UserDetail { ConnectionId = id, UserName = userName });
+
+                // send to caller
+                Clients.Caller.onConnected(id, userName, ConnectedUsers, CurrentMessage);
+
+                // send to all except caller client
+                Clients.AllExcept(id).onNewUserConnected(id, userName);
+
+            }
+
+        }
+
+        public void SendMessageToAll(string userName, string message)
+        {
+            // store last 100 messages in cache
+            AddMessageinCache(userName, message);
+
+            // Broad cast message
+            Clients.All.messageReceived(userName, message);
+        }
+
+        public void SendPrivateMessage(string toUserId, string message)
+        {
+
+            string fromUserId = Context.ConnectionId;
+
+            var toUser = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == toUserId);
+            var fromUser = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == fromUserId);
+
+            if (toUser != null && fromUser != null)
+            {
+                // send to 
+                Clients.Client(toUserId).sendPrivateMessage(fromUserId, fromUser.UserName, message);
+
+                // send to caller user
+                Clients.Caller.sendPrivateMessage(toUserId, fromUser.UserName, message);
+            }
+
+        }
+
+        // nzn kaip issaukt nei is cliento, nei is srv. automatiskai kvieciasi
+        public override System.Threading.Tasks.Task OnDisconnected(bool t)
+        {
+            if (t)
+            {
+                var item = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+                if (item != null)
+                {
+                    ConnectedUsers.Remove(item);
+
+                    var id = Context.ConnectionId;
+                    Clients.All.onUserDisconnected(id, item.UserName);
+
+                    Clients.All.messageReceived(item.UserName, "Has disconnected");
+                }
+            }
+
+            else
+            {
+                var item = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+                if (item != null)
+                {
+                    ConnectedUsers.Remove(item);
+
+                    var id = Context.ConnectionId;
+                    Clients.All.onUserDisconnected(id, item.UserName);
+
+                    Clients.All.messageReceived(item.UserName, "Has disconnected (lost connection)");
+                }
+            }
+
+            return base.OnDisconnected(t);
+        }
+
+
+        private void AddMessageinCache(string userName, string message)
+        {
+            CurrentMessage.Add(new MessageDetail { UserName = userName, Message = message });
+
+            if (CurrentMessage.Count > 100)
+                CurrentMessage.RemoveAt(0);
+        }
+    }
+}
--- BaBookStudentai/Startup.cs
+++ BaBookStudentai/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.Owin;
 using Owin;
 using BaBookStudentai.Models;
 using BaBookStudentai.Entities;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
 
 [assembly: OwinStartup(typeof(BaBookStudentai.Startup))]
 
@@ -26,6 +28,27 @@ namespace BaBookStudentai
             //    db.Group.Add(group);
             //    db.SaveChanges();
             //}
+
+            // Branch the pipeline here for requests that start with "/signalr"
+            app.Map("/signalr", map =>
+            {
+                // Setup the CORS middleware to run before SignalR.
+                // By default this will allow all origins. You can 
+                // configure the set of origins and/or http verbs by
+                // providing a cors options with a different policy.
+                map.UseCors(CorsOptions.AllowAll);
+                var hubConfiguration = new HubConfiguration
+                {
+                    // You can enable JSONP by uncommenting line below.
+                    // JSONP requests are insecure but some older browsers (and some
+                    // versions of IE) require JSONP to work cross domain
+                    EnableJSONP = true
+                };
+                // Run the SignalR pipeline. We're not using MapSignalR
+                // since this branch already runs under the "/signalr"
+                // path.
+                map.RunSignalR(hubConfiguration);
+            });
 
         }
         
